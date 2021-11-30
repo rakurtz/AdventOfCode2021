cd78003d8503237a75ed77cf5faac80177464041
martin <martin.kurtz@icloud.com>
2021-11-30 18:13:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Advent of Code 2021
+# empty profile for new day 1
+
+with open('inputfiles/___.txt', "r") as f:
+    data = f.read().splitlines()
+
+
+def part1():
+    """ first part of puzzle"""
+    pass
+
+
+def part2():
+    """ first part of puzzle"""
+    pass
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
