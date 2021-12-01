f7e091dce343c51ef5b2a5c952faf1e9004f47ef
martin <martin.kurtz@icloud.com>
2021-12-01 20:58:43 +0100

    Day 1 completed -- known issue: in part2() i don't make sure, that the sliding window (sum_of_first/second_three stays within the range of data[]. It doesnt throw an exception since the notation of data[from:to] doesn't mind about the end parameter being out of range. But this way it could happen the results aren't correct.
    -- how to improve?
    maybe there is a better way to create a "sliding window" to sweep through a list.

--- day1.py
+++ day1.py
@@ -1,27 +1,39 @@
 # Advent of Code 2021
-# empty profile for new day 1
+# Day 1 - Part I + II
 
 import timeit
 
 with open('inputfiles/day1.txt', "r") as f:
-    data = f.read().splitlines()
+    data = list(map(lambda x: int(x), f.read().splitlines()))
 
 
 def part1():
     """ first part of puzzle"""
+    counter = 0
+    for x in range(1, len(data)):
+        if data[x] > data[x-1]:
+            counter += 1
+    return counter
 
 
 def part2():
     """ second part of puzzle"""
+    counter = 0
+    for x in range(0, len(data)):
+        sum_of_first_three = sum(data[x:x+3])
+        sum_of_second_three = sum(data[x+1:x+4])
+        if sum_of_second_three > sum_of_first_three:
+            counter += 1
+    return counter
 
 
 if __name__ == "__main__":
     start = timeit.default_timer()
-    part1()
-    part2()
+    print(part1())
+    print(part2())
     stop = timeit.default_timer()
 
     print(f"\nTook {stop - start} seconds to finish")
 
-
-
+    # part 1: 1759 - Took 0.0004945829999999998 seconds to finish
+    # part 1: 1805 - Took 0.0008386659999999983 seconds to finish
