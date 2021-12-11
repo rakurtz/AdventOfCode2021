ad6e07924960dafc40c8b7bbdcd1d36a778ef0df
martin <martin.kurtz@icloud.com>
2021-12-12 00:15:22 +0100

    Day 7
    Part I
    - using statistics.median() to find optimal horizontal point
    
    Part II
    - now uses the previously unused function, that numerical finds most fuel efficient horizontal point
    - function that calculates fuel for given reference point has mod_increased now and makes use of Gauß's formula to sum numbers from a to b.

--- day7.py
+++ day7.py
@@ -5,20 +5,28 @@ import timeit
 from statistics import median
 
 
-def calculate_fuel_for_point(horizontal_positions: list, reference_position: int):
+def calculate_fuel_for_point(horizontal_positions: list, reference_position: int, mod_increase=False):
+    """ calculates and returnes burned fuel for all crabs moving to reference point """
+    """ with mod_increase=True it uses calculation for Part II of puzzle"""
+
     fuel = 0
     for crab in horizontal_positions:
-        fuel += abs(crab - reference_position)
+        if not mod_increase:
+            fuel += abs(crab - reference_position)
+        else:
+            # using Gauß's formula for Part II
+            n = abs(crab - reference_position)
+            fuel += n * (n + 1) / 2
+
     return fuel
 
 
-def calculate_lowest_fuel(horizontal_positions: list):
+def calculate_lowest_fuel_with_mod_increase(horizontal_positions: list):
     """ Numeric solution of finding the median, returns a tuple of fuel used for optimal position """
-    """ not used in this program """
 
     fuels = []
     for position in range(min(horizontal_positions), max(horizontal_positions)):
-        fuels.append((calculate_fuel_for_point(horizontal_positions, position), position))
+        fuels.append((calculate_fuel_for_point(horizontal_positions, position, mod_increase=True), position))
 
     return min(fuels)
 
@@ -30,10 +38,12 @@ if __name__ == "__main__":
         horizontal_positions = list(map(lambda x: int(x), f.read().split(",")))
 
     print(f"Solution Part I: {calculate_fuel_for_point(horizontal_positions, median(horizontal_positions))}")
-
+    print(f"Solution Part II: {calculate_lowest_fuel_with_mod_increase(horizontal_positions)}")
     stop = timeit.default_timer()
 
     print(f"Took {stop - start} seconds to finish\n")
 
-
+    # Part I:                            340987
+    # Part II:                         96987874
+    # All together:     Took 0.1668858340708539 seconds to finish
 
